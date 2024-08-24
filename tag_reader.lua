3d767bf70c56d00f6844081130e06fefd67bd60f
Jason-J-Hunt <huntjason@live.com>
2024-08-24 17:54:46 -0600

    test

--- tag_reader.lua
+++ tag_reader.lua
@@ -216,40 +216,39 @@ function scanNearestBlock(scanner, radius)
 
 end
 
-function main()
-    -- Enter the main loop
-    local continueOperation = true
-    while continueOperation do
-        -- Print the header
-        printHeader(scanner)
+-- Enter the main loop
+local continueOperation = true
+while continueOperation do
+    -- Print the header
+    printHeader(scanner)
 
-        -- Print the instructions
-        print("Actions:")
-        print("[s] View Nearby Blocks")
-        print("[w] Scan Nearest Block")
+    -- Print the instructions
+    print("Actions:")
+    print("[s] View Nearby Blocks")
+    print("[w] Scan Nearest Block")
 
-        print("\nSettings:")
-        print("[a] Decrease Scan Radius")
-        print("[d] Increase Scan Radius")
+    print("\nSettings:")
+    print("[a] Decrease Scan Radius")
+    print("[d] Increase Scan Radius")
 
-        print("\n[q] Quit")
+    print("\n[q] Quit")
 
-        -- Wait for a key press
-        local event, key = os.pullEvent("key")
+    -- Wait for a key press
+    local event, key = os.pullEvent("key")
 
-        -- Check if the key is 'q'
-        if key == keys.q then
-            -- Exit
-            continueOperation = false
-        elseif key == keys.s then
-            -- Scan for all ores in the area
-            scanArea(scanner, SCAN_RADIUS)
-        elseif key == keys.w then
-            -- Scan for the nearest ore
-            scanNearestBlock(scanner, SCAN_RADIUS)
-        end
+    -- Check if the key is 'q'
+    if key == keys.q then
+        -- Exit
+        continueOperation = false
+    elseif key == keys.s then
+        -- Scan for all ores in the area
+        scanArea(scanner, SCAN_RADIUS)
+    elseif key == keys.w then
+        -- Scan for the nearest ore
+        scanNearestBlock(scanner, SCAN_RADIUS)
     end
 end
+
 
 print("hello")
 
