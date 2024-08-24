723c6143484180c346ea7a4d872f3a9988c95499
Jason-J-Hunt <huntjason@live.com>
2024-08-24 17:59:11 -0600

    fix

--- tag_reader.lua
+++ tag_reader.lua
@@ -10,9 +10,6 @@ local PULSE_COOLDOWN = 2
 -- Whether to use the `PULSE_COOLDOWN` or the `scanner.getScanCooldown()` function value for the pulse cooldown time.
 local MANUAL_PULSE_COOLDOWN = true
 
--- Link with the scanner
-local scanner = peripheral.find("geoScanner")
-
 -- Clears the screen and prints the header again
 -- scanner: A Geo Scanner peripheral.
 function printHeader(scanner)
@@ -216,36 +213,40 @@ function scanNearestBlock(scanner, radius)
 
 end
 
--- Enter the main loop
-local continueOperation = true
-while continueOperation do
-    -- Print the header
-    printHeader(scanner)
+function main()
+    -- Link with the scanner
+    local scanner = peripheral.find("geoScanner")
+    -- Enter the main loop
+    local continueOperation = true
+    while continueOperation do
+        -- Print the header
+        printHeader(scanner)
 
-    -- Print the instructions
-    print("Actions:")
-    print("[s] View Nearby Blocks")
-    print("[w] Scan Nearest Block")
+        -- Print the instructions
+        print("Actions:")
+        print("[s] View Nearby Blocks")
+        print("[w] Scan Nearest Block")
 
-    print("\nSettings:")
-    print("[a] Decrease Scan Radius")
-    print("[d] Increase Scan Radius")
+        print("\nSettings:")
+        print("[a] Decrease Scan Radius")
+        print("[d] Increase Scan Radius")
 
-    print("\n[q] Quit")
+        print("\n[q] Quit")
 
-    -- Wait for a key press
-    local event, key = os.pullEvent("key")
+        -- Wait for a key press
+        local event, key = os.pullEvent("key")
 
-    -- Check if the key is 'q'
-    if key == keys.q then
-        -- Exit
-        continueOperation = false
-    elseif key == keys.s then
-        -- Scan for all ores in the area
-        scanArea(scanner, SCAN_RADIUS)
-    elseif key == keys.w then
-        -- Scan for the nearest ore
-        scanNearestBlock(scanner, SCAN_RADIUS)
+        -- Check if the key is 'q'
+        if key == keys.q then
+            -- Exit
+            continueOperation = false
+        elseif key == keys.s then
+            -- Scan for all ores in the area
+            scanArea(scanner, SCAN_RADIUS)
+        elseif key == keys.w then
+            -- Scan for the nearest ore
+            scanNearestBlock(scanner, SCAN_RADIUS)
+        end
     end
 end
 
